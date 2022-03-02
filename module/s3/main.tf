349122a871afb87b607eaad40279335c9b3d23d4
VivekSayOne1 <89903842+VivekSayOne1@users.noreply.github.com>
2022-03-02 12:00:51 +0530

    Update main.tf

--- module/s3/main.tf
+++ module/s3/main.tf
@@ -27,22 +27,22 @@ resource "aws_s3_bucket_website_configuration" "s3-website" {
     key = "error.html"
   }
 
-  routing_rule {
-    condition {
-      key_prefix_equals = "docs/"
-    }
-    redirect {
-      replace_key_prefix_with = "documents/"
-    }
-  }
+  #routing_rule {
+   # condition {
+    #  key_prefix_equals = "docs/"
+   # }
+    #redirect {
+     # replace_key_prefix_with = "documents/"
+   # }
+  #}
 }
 
-resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = aws_s3_bucket.terraform-s3.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+#resource "aws_s3_bucket_versioning" "versioning_example" {
+  #bucket = aws_s3_bucket.terraform-s3.id
+  #versioning_configuration {
+   # status = "Enabled"
+  #}
+#}
 resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
   bucket = aws_s3_bucket.terraform-s3.id
   policy = data.aws_iam_policy_document.allow_access_from_another_account.json
